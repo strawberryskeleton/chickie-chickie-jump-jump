f069707492e29a24fbef8a7ddfde50f0489fdb6e
StrawberrySkeleton <xradhi5685@gmail.com>
2026-06-27 13:29:37 +0530

    check collision logic completed

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const endScreen = document.getElementById('end-screen')
 const chickie = document.getElementById('chickie')
 const egg = document.getElementById('egg')
 
+const rootFontSize = parseFloat(window.getComputedStyle(document.documentElement).fontSize);
+
 let score = 0
 let gameActive = true
 // let lastTime = null
@@ -45,3 +47,23 @@ function jump () {
         chickie.classList.remove('jump')
     }, 500)
 }
+
+const isAlive = setInterval(() => {
+    let chickieTop = parseInt(window.getComputedStyle(chickie).getPropertyValue("top"))
+    let eggLeft = parseInt(window.getComputedStyle(egg).getPropertyValue("left"))
+
+    let chickieLeftPx = 5 * rootFontSize
+    let chickieRightPx = (5+6) * rootFontSize
+    let eggLeftPx = 5 * rootFontSize
+    let eggRightPx = eggLeft + (3 * rootFontSize)
+
+    let groundLevel = 23.5 * rootFontSize
+
+    if ((eggRightPx > chickieLeftPx && eggLeft < chickieRightPx) && (chickieTop >= groundLevel)) {
+        alert('game over')
+        egg.style.animation = 'none'
+        clearInterval(isAlive)
+        gameActive = false
+    }
+
+}, 10);
